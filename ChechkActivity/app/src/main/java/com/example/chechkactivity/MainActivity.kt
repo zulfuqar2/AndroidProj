99a50810f57ee1ad17c85e342e8ef1b847667812
zulfuqar2 <anarzulfuqar2005@gmail.com>
2023-09-18 17:33:37 +0400

    Just make login page

---
+++ ChechkActivity/app/src/main/java/com/example/chechkactivity/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.chechkactivity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chechkactivity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var binding :ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+
+            val Intent =Intent(this@MainActivity,MainActivity2::class.java)
+            startActivity(Intent)
+            finish()
+//            val intent = Intent(this, MainActivity2::class.java)
+//            startActivity(intent)
+
+
+
+        }
+    }
+}
---
+++ ChechkActivity/app/src/main/java/com/example/chechkactivity/MainActivity2.kt
@@ -0,0 +1,15 @@
+package com.example.chechkactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chechkactivity.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
