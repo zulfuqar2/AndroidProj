35f32e55859610223cb94da214b1204624b346fe
zulfuqar2 <anarzulfuqar2005@gmail.com>
2023-09-27 18:17:44 +0400

    Every thnigs are rady for presents but we have 1 bug but excalty i will fix

--- ChechkActivity/app/src/main/java/com/example/chechkactivity/MainActivity5.kt
+++ ChechkActivity/app/src/main/java/com/example/chechkactivity/MainActivity5.kt
@@ -1,8 +1,10 @@
 package com.example.chechkactivity
-
+import android.widget.ImageView
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import com.example.chechkactivity.databinding.ActivityMain5Binding
 
@@ -12,6 +14,33 @@ class MainActivity5 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding=ActivityMain5Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+
+        val imageViews = arrayOf<ImageView>(
+            binding.imageView18, binding.imageView19
+        )
+
+        for (imageView in imageViews) {
+            imageView.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+
+                    if (imageView.visibility == View.INVISIBLE) {
+                        imageView.visibility = View.VISIBLE
+                    } else {
+                        imageView.visibility = View.INVISIBLE
+                    }
+                }
+            })
+        }
+
+//         binding.bagca3.setOnClickListener(object : View.OnClickListener {
+//
+//         })
+
+
+
 
       binding.exitced.setOnClickListener {
 
@@ -44,12 +73,10 @@ class MainActivity5 : AppCompatActivity() {
 
         }
 
-//        binding.imgtell.setOnClickListener {
-//            Toast.makeText(applicationContext,"This Function Coming soon ",Toast.LENGTH_SHORT).show()
-//
-//
-//
-//        }
+        binding.tesdiq.setOnClickListener {
+           Toast.makeText(applicationContext,"This Function Coming soon ",Toast.LENGTH_SHORT).show()
+
+        }
 
 
 
@@ -57,4 +84,8 @@ class MainActivity5 : AppCompatActivity() {
 
 
     }
+
+
+
+
 }
